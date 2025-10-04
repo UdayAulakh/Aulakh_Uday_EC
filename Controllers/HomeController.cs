31c9d120917666cd775ad8e284a1403143b12672
udayaulakh-08 <udayaulakh@utexas.edu>
2025-10-03 22:53:10 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    // Home page
+    public IActionResult Index()
+    {
+        return View(); // Loads Views/Home/Index.cshtml
+    }
+
+    // Resume page
+    public IActionResult Resume()
+    {
+        return View(); // Loads Views/Home/Resume.cshtml
+    }
+}
